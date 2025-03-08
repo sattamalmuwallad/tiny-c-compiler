43ed5662fb3648631a7e6a86090b73da8bee88b3
Sattam Almuwallad <sattamalmuwallad@gmail.com>
2025-03-08 13:59:32 +0000

    testing the compiler

--- project/tinybasic_compiler/teenytiny.py
+++ project/tinybasic_compiler/teenytiny.py
@@ -4,6 +4,6 @@ def main():
     source = "LET foobar = 123"
     lexer = Lexer(source)
     while lexer.peek() != '\0':
-        print(lexer.curchar())
+        print(lexer.curchar)
         lexer.nextChar()
 main()
